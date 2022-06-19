308022864d719ab69f41b0ad3b977c2d92d86f2e
Lawrence <lawrence.budzisz@gmail.com>
2022-06-19 21:56:07 +0100

    Blog submit

--- src/NotFound.js
+++ src/NotFound.js
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className="not-found">
       <h2>Sorry</h2>
       <p>That page cannot be found</p>
-      <Link to="/">Back to Homepage</Link>
+      <Link to="/">Back to Homepage...</Link>
     </div>
   );
 }
